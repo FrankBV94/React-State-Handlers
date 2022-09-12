a3626dc94eca7baa5e67394f22559bf67e747de1
FrankBV94 <frankbotelle@gmail.com>
2022-09-12 13:29:27 -0400

    Subject Manager

---
+++ react-state/src/utilities/subject-manager.js
@@ -0,0 +1,25 @@
+import { Subject } from "rxjs";
+
+/**
+ * Los "Subject" pueden enviar y recibir informacion porque son "Observables" y "Observers' al mismo tiempo
+ */
+export class SubjectManager {
+  subject = new Subject();
+  /**
+   * Devuelve el subject como un simple observable para no romper el encapsulamiento
+   * Si se devuelve "subject" se puede ejecutar subject.next(data) para enviar informacion fuera de los
+   * metodos definidos por la clase(encapsulamiento)
+   * Solo lee informacion
+   *
+   */
+  get getSubject() {
+    return this.subject.asObservable();
+  }
+
+  /**
+   * Solo envia informacion
+   */
+  set setSubject(value) {
+    this.subject.next(value);
+  }
+}
