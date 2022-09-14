3ab689e56a3eead8fd198901a2598af6148e3641
FrankBV94 <frankbotelle@gmail.com>
2022-09-14 10:36:03 -0400

    RXJS Ok

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Component1 from './components/Component1'
-import Component2 from './components/Component2'
+import "./App.css";
+import Component1 from "./components/RxJS/Component1";
+import Component2 from "./components/RxJS/Component2";
 
 function App() {
   return (
     <div className="App">
-      <Component1/>
-      <Component2/>
+      <Component1 />
+      <Component2 />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-state/src/services/sharingInformationService.js
+++ react-state/src/services/sharingInformationService.js
@@ -4,4 +4,4 @@ import { SubjectManager } from "../utilities/subject-manager";
  * Cualquiera que utilice el "sharingInformationService" va a acceder al mismo canal de comunicacion
  * 
  */
-export default sharingInformationService = new SubjectManager();
+export const sharingInformationService = new SubjectManager();
